2aa6baf4e942291c1399192ab265d2f05b46d76f
vezeveer <mortalmanny@live.com>
2019-06-15 17:18:48 +0800

    workable state

--- App/Main.js
+++ App/Main.js
@@ -1,142 +1,17 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Dimensions, Button, Text, Image } from "react-native";
-
 import AutoHeightImage from 'react-native-auto-height-image';
 import { NativeRouter, Route, Link } from "react-router-native";
-
 import SoundPlayer from 'react-native-sound-player'
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
-} from 'react-native-svg';
 
-const img1 = require('../img/169.jpg')
 const screenWidth = Math.round(Dimensions.get('window').width)
 const words = require('../dictionary/testDict.json')
 
 
-class One extends Component {
-
-  render() {
-    return (
-      <View>
-        <View style={styles.subNavCollection}>
-          <Link
-            to={`${this.props.match.url}/rendering`}
-            style={styles.subNavItem}
-            underlayColor="#547D9C"
-          >
-            <Text>Rendering with React</Text>
-          </Link>
-          <Link
-            to={`${this.props.match.url}/components`}
-            style={styles.subNavItem}
-            underlayColor="#547D9C"
-          >
-            <Text>Components</Text>
-          </Link>
-          <Link
-            to={`${this.props.match.url}/props-v-state`}
-            style={styles.subNavItem}
-            underlayColor="#547D9C"
-          >
-            <Text>Props v. State</Text>
-          </Link>
-        </View>
-
-
-
-        <Route path={`${this.props.match.url}/:topicId`} component={Topic} />
-        <Route
-          exact
-          path={this.props.match.url}
-          render={() => <View style={styles.selectWord}><Text>Select Word</Text></View>}
-        />
-      </View>
-    );
-  }
-}
-
-// ONE ENDS HERE ########
-
-function Two({ match }) {
-  return (
-    <View>
-      <View style={styles.subNavCollection}>
-        <Link
-          to={`${match.url}/rendering`}
-          style={styles.subNavItem}
-          underlayColor="#547D9C"
-        >
-          <Text>Rendering with React</Text>
-        </Link>
-        <Link
-          to={`${match.url}/components`}
-          style={styles.subNavItem}
-          underlayColor="#547D9C"
-        >
-          <Text>Components</Text>
-        </Link>
-        <Link
-          to={`${match.url}/props-v-state`}
-          style={styles.subNavItem}
-          underlayColor="#547D9C"
-        >
-          <Text>Props v. State</Text>
-        </Link>
-      </View>
-
-
-
-      <Route path={`${match.url}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.url}
-        render={() => <View style={styles.selectWord}><Text>Select Word</Text></View>}
-      />
-    </View>
-  );
-}
-
-// TWO ENDS HERE #########
-
-const Topic = ({ match }) => {
-  console.log("Topic went thru.")
-  return (
-    <View>
-      <Text style={styles.topic}>{match.params.topicId}</Text>
-      <Text style={styles.topic}>{match.params.topicId}</Text>
-      <Text>YOLO</Text>
-    </View>
-  );
-}
-
-class ProcessWord extends React.Component {
-
-  state = {
-    currentWord: 'chair'
-  }
+class RenderImageAndAudio extends Component {
 
   playAudio = () => {
     try {
-      // play the file tone.mp3
       SoundPlayer.playSoundFile(this.props.match.params.wordId, 'mp3')
       // or play from url
       //SoundPlayer.playUrl('https://example.com/music.mp3')
@@ -145,43 +20,27 @@ class ProcessWord extends React.Component {
     }
   }
 
-  findWord = () => {
-    console.log("find word went thru...")
-    const word = this.calcWord();
-    //console.log("THe Word IS: ", word)
-    // this.setState({
-    //   currentWord: word
-    // })
-  }
-
-  calcWord = () => {
+  // Find word in db then return image data
+  findImageData = () => {
     let word = this.props.match.params.wordId;
-    console.log("WordId: ", word)
-    let keys = Object.keys(words.words);
     for (let i = 0; i <= words.words.length; i++) {
       try {
         if (Object.keys(words.words[i])[0] === word) {
-          console.log("KEY FOUND: ", Object.keys(words.words[i])[0])
-          let wordFound = Object.keys(words.words[i])[0];
-          return words.words[i][wordFound].uri;
-          //console.log("CalcWord returned: ", words.words[word].uri)
-          //return words.words[word].uri
+          return words.words[i][Object.keys(words.words[i])[0]].uri;
         }
       } catch (err) {
-        console.log("Error at calcWord for loop")
+        console.log("Error at findImageData for loop")
       }
     }
   }
 
   render() {
-    //this.findWord();
-    console.log("From console render: ", this.state.currentWord)
     return (
       <View>
         <AutoHeightImage
           width={screenWidth - 20}
           source={{
-            uri: this.calcWord()
+            uri: this.findImageData()
           }}
         />
         <Text style={styles.wordLabel}>{this.props.match.params.wordId}</Text>
@@ -192,24 +51,23 @@ class ProcessWord extends React.Component {
   }
 }
 
-class Three extends React.Component {
+class WordMenu extends Component {
 
   state = {
     currentWords: '',
     iii: ["chair", "bridge", "house"],
-    ii: ["building", "door", "table"]
+    ii: ["building", "door", "table"],
+    i: ["car", "tree", "cat", "dog"]
   }
 
-  componentWillMount() {
-    let { wordsId } = this.props.match.params
+  static getDerivedStateFromProps(props, state) {
+    let { wordsId } = props.match.params
     if (wordsId === 'iii') {
-      this.setState({
-        currentWords: this.state.iii
-      })
+      return { currentWords: state.iii }
     } else if (wordsId === 'ii') {
-      this.setState({
-        currentWords: this.state.ii
-      })
+      return { currentWords: state.ii }
+    } else if (wordsId === 'i') {
+      return { currentWords: state.i }
     }
   }
 
@@ -219,30 +77,29 @@ class Three extends React.Component {
       <View>
         <View style={styles.subNavCollection}>
           <Link
-            to={`${this.props.match.url}/chair`}
+            to={`${this.props.match.url}/${this.state.currentWords[0]}`}
             style={styles.subNavItem}
             underlayColor="#547D9C"
           >
-            <Text>chair</Text>
+            <Text>{this.state.currentWords[0]}</Text>
           </Link>
           <Link
-            to={`${this.props.match.url}/bridge`}
+            to={`${this.props.match.url}/${this.state.currentWords[1]}`}
             style={styles.subNavItem}
             underlayColor="#547D9C"
           >
-            <Text>bridge</Text>
+            <Text>{this.state.currentWords[1]}</Text>
           </Link>
           <Link
-            to={`${this.props.match.url}/house`}
+            to={`${this.props.match.url}/${this.state.currentWords[2]}`}
             style={styles.subNavItem}
             underlayColor="#547D9C"
           >
-
-            <Text>house</Text>
+            <Text>{this.state.currentWords[2]}</Text>
           </Link>
         </View>
 
-        <Route path={`${this.props.match.url}/:wordId`} component={ProcessWord} />
+        <Route path={`${this.props.match.url}/:wordId`} component={RenderImageAndAudio} />
         <Route
           exact
           path={this.props.match.url}
@@ -253,18 +110,6 @@ class Three extends React.Component {
   }
 }
 
-// THREE ENDS HERE #############
-
-const Choose = () => {
-  return (
-    <View style={styles.pleaseSelect}>
-      <Text style={{ fontSize: 18 }}>Select any</Text>
-
-    </View>
-
-  )
-}
-
 export default class App extends Component {
 
   render() {
@@ -272,23 +117,26 @@ export default class App extends Component {
       <NativeRouter>
         <View style={styles.container}>
           <View style={styles.nav}>
-            <Link to="/One" underlayColor="#547D9C" style={styles.navItem}>
+            <Link to="/wordmenu/i" underlayColor="#547D9C" style={styles.navItem}>
               <Text>I</Text>
             </Link>
-            <Link to="/Two" underlayColor="#547D9C" style={styles.navItem}>
+            <Link to="/wordmenu/ii" underlayColor="#547D9C" style={styles.navItem}>
               <Text>II</Text>
             </Link>
-            <Link to="/Three/iii" underlayColor="#547D9C" style={styles.navItem}>
+            <Link to="/wordmenu/iii" underlayColor="#547D9C" style={styles.navItem}>
               <Text>III</Text>
             </Link>
           </View>
 
 
-          <Route exact path="/" component={Choose} />
-          {/*<Route exact path="/" component={Home} />*/}
-          <Route path="/One" component={One} />
-          <Route path="/Two" component={Two} />
-          <Route path="/Three/:wordsId" component={Three} />
+          <Route exact path="/" component={() => {
+            return (
+              <View style={styles.pleaseSelect}>
+                <Text style={{ fontSize: 18 }}>Select any</Text>
+              </View>
+            )
+          }} />
+          <Route path="/wordmenu/:wordsId" component={WordMenu} />
         </View>
       </NativeRouter>
     );
